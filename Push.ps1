370c33d91e184d6c6de2562d910e933336e81aff
Andrew Piper <andrew@threax.com>
2021-05-18 15:45:03 -0400

    Updated push to send everything.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
+git push
 git push --tags
+git push https://github.com/threax/HtmlRapier.Compile.git
 git push https://github.com/threax/HtmlRapier.Compile.git --tags
+git push https://gitlab.com/threax-software/HtmlRapier.Compile.git
 git push https://gitlab.com/threax-software/HtmlRapier.Compile.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Compile
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.Compile --tags
